status-im/status-mobile

commit bddae03ab234f6574b0c5d4615f586e81512a638

Allow multiple push notifications
We keep tokens synchronized across devices, so that the user can notify
us on any paired device.
Currently we record the installation id associated to the fcm-token even
though is not necessary, but it will be once we send device-to-device
messages, in which case we want to notify only those devices.

Signed-off-by: Andrea Maria Piana <andrea.maria.piana@gmail.com>

src/status_im/data_store/contacts.cljs
@@ -8,6 +8,9 @@
   (-> contact
       (update :tags #(into #{} %))))
 
+(defn- serialize-contact [contact]
+  (update contact :device-info #(or (vals %) [])))
+
 (re-frame/reg-cofx
  :data-store/get-all-contacts
  (fn [coeffects _]
@@ -22,7 +25,7 @@
   (fn [realm]
     (core/create realm
                  :contact
-                 contact
+                 (serialize-contact contact)
                  true)))
 
 (defn save-contacts-tx

src/status_im/accounts/update/core.cljs
@@ -1,15 +1,23 @@
 (ns status-im.accounts.update.core
-  (:require [status-im.data-store.accounts :as accounts-store]
-            [status-im.transport.message.protocol :as protocol]
-            [status-im.data-store.transport :as transport-store]
-            [status-im.transport.message.contact :as message.contact]
-            [status-im.utils.fx :as fx]))
+  (:require
+   [status-im.contact.device-info :as device-info]
+   [status-im.data-store.accounts :as accounts-store]
+   [status-im.data-store.transport :as transport-store]
+   [status-im.transport.message.protocol :as protocol]
+   [status-im.transport.message.contact :as message.contact]
+   [status-im.utils.fx :as fx]))
 
-(fx/defn account-update-message [{:keys [db]}]
+(fx/defn account-update-message [{:keys [db] :as cofx}]
   (let [account (:account/account db)
         fcm-token (get-in db [:notifications :fcm-token])
         {:keys [name photo-path address]} account]
-    (message.contact/ContactUpdate. name photo-path address fcm-token)))
+    (message.contact/ContactUpdate. name photo-path address fcm-token (device-info/all cofx))))
+
+(fx/defn send-account-update [cofx]
+  (protocol/send
+   (account-update-message cofx)
+   nil
+   cofx))
 
 (fx/defn send-contact-update-fx
   [{:keys [db] :as cofx} chat-id payload]
@@ -47,7 +55,6 @@
   [{:keys [db] :as cofx} new-account-fields {:keys [success-event]}]
   (let [current-account (:account/account db)
         new-account     (merge current-account new-account-fields)
-        fcm-token       (get-in db [:notifications :fcm-token])
         fx              {:db                 (assoc db :account/account new-account)
                          :data-store/base-tx [{:transaction (accounts-store/save-account-tx new-account)
                                                :success-event success-event}]}
@@ -55,7 +62,7 @@
     (if (or (:name new-account-fields) (:photo-path new-account-fields))
       (fx/merge cofx
                 fx
-                #(protocol/send (account-update-message %) nil %))
+                #(send-account-update %))
       fx)))
 
 (fx/defn clean-seed-phrase

src/status_im/chat/models/message.cljs
@@ -382,14 +382,14 @@
               (add-own-status chat-id message-id :sending)
               (send chat-id message-id wrapped-record))))
 
-(fx/defn send-push-notification [cofx chat-id message-id fcm-token status]
-  (log/debug "#6772 - send-push-notification" message-id fcm-token)
-  (when (and fcm-token (= status :sent))
+(fx/defn send-push-notification [cofx chat-id message-id fcm-tokens status]
+  (log/debug "#6772 - send-push-notification" message-id fcm-tokens)
+  (when (and (seq fcm-tokens) (= status :sent))
     (let [payload {:from (accounts.db/current-public-key cofx)
                    :to chat-id
                    :id message-id}]
       {:send-notification {:data-payload (notifications/encode-notification-payload payload)
-                           :tokens       [fcm-token]}})))
+                           :tokens       fcm-tokens}})))
 
 (fx/defn update-message-status [{:keys [db]} chat-id message-id status]
   (let [from           (get-in db [:chats chat-id :messages message-id :from])

src/status_im/contact/core.cljs
@@ -2,7 +2,9 @@
   (:require [re-frame.core :as re-frame]
             [status-im.accounts.db :as accounts.db]
             [status-im.chat.models :as chat.models]
+            [clojure.set :as clojure.set]
             [status-im.contact.db :as contact.db]
+            [status-im.contact.device-info :as device-info]
             [status-im.data-store.contacts :as contacts-store]
             [status-im.data-store.messages :as data-store.messages]
             [status-im.data-store.chats :as data-store.chats]
@@ -48,6 +50,7 @@
     {:name          name
      :profile-image photo-path
      :address       address
+     :device-info   (device-info/all {:db db})
      :fcm-token     fcm-token}))
 
 (fx/defn upsert-contact [{:keys [db] :as cofx}
@@ -218,7 +221,7 @@
 (defn handle-contact-update
   [public-key
    timestamp
-   {:keys [name profile-image address fcm-token] :as m}
+   {:keys [name profile-image address fcm-token device-info] :as m}
    {{:contacts/keys [contacts] :as db} :db :as cofx}]
   ;; We need to convert to timestamp ms as before we were using now in ms to
   ;; set last updated
@@ -243,15 +246,15 @@
                                :address      (or address
                                                  (:address contact)
                                                  (contact.db/public-key->address public-key))
+                               :device-info  (device-info/merge-info
+                                              timestamp
+                                              (:device-info contact)
+                                              device-info)
                                :last-updated timestamp-ms
-                                  ;;NOTE (yenda) in case of concurrent contact request
+                                ;;NOTE (yenda) in case of concurrent contact request
                                :pending?     (get contact :pending? true)}
                                fcm-token (assoc :fcm-token fcm-token))]
-        ;;NOTE (yenda) only update if there is changes to the contact
-        (when-not (= contact-props
-                     (select-keys contact [:public-key :address :photo-path
-                                           :name :fcm-token :pending?]))
-          (upsert-contact cofx contact-props))))))
+        (upsert-contact cofx contact-props)))))
 
 (def receive-contact-request handle-contact-update)
 (def receive-contact-request-confirmation handle-contact-update)

src/status_im/contact/device_info.cljs
@@ -1,0 +1,26 @@
+(ns status-im.contact.device-info
+  (:require [status-im.utils.config :as config]))
+
+(defn all [{:keys [db]}]
+  (filter
+   :fcm-token
+   (conj
+    (->> (:pairing/installations db)
+         (vals)
+         (filter :enabled?)
+         (filter :fcm-token)
+         (take config/max-installations)
+         (map #(hash-map :id (:installation-id %)
+                         :fcm-token (:fcm-token %))))
+    {:id (get-in db [:account/account :installation-id])
+     :fcm-token (get-in db [:notifications :fcm-token])})))
+
+(defn merge-info [timestamp previous-devices new-devices]
+  (reduce (fn [acc {:keys [id] :as new-device}]
+            (if (:fcm-token new-device)
+              (assoc acc
+                     id
+                     (assoc new-device :timestamp timestamp))
+              acc))
+          previous-devices
+          new-devices))

src/status_im/data_store/realm/schemas/account/contact.cljs
@@ -93,3 +93,7 @@
                       :description      {:type :string :optional true}
                       :public-key       :string
                       :tags             {:type     "string[]"}}})
+
+(def v5 (assoc-in v4 [:properties :device-info]
+                  {:type       :list
+                   :objectType :contact-device-info}))

src/status_im/data_store/realm/schemas/account/contact_device_info.cljs
@@ -1,0 +1,7 @@
+(ns status-im.data-store.realm.schemas.account.contact-device-info)
+
+(def v1 {:name :contact-device-info
+         :primaryKey :id
+         :properties {:id :string
+                      :timestamp :int
+                      :fcm-token :string}})

src/status_im/data_store/realm/schemas/account/core.cljs
@@ -6,6 +6,7 @@
             [status-im.data-store.realm.schemas.account.contact :as contact]
             [status-im.data-store.realm.schemas.account.message :as message]
             [status-im.data-store.realm.schemas.account.user-status :as user-status]
+            [status-im.data-store.realm.schemas.account.contact-device-info :as contact-device-info]
             [status-im.data-store.realm.schemas.account.local-storage :as local-storage]
             [status-im.data-store.realm.schemas.account.mailserver :as mailserver]
             [status-im.data-store.realm.schemas.account.browser :as browser]
@@ -413,6 +414,21 @@
           dapp-permissions/v9
           contact-recovery/v1])
 
+(def v37 [chat/v14
+          transport/v8
+          contact/v5
+          message/v9
+          mailserver/v11
+          mailserver-topic/v1
+          user-status/v2
+          membership-update/v1
+          installation/v3
+          local-storage/v1
+          browser/v8
+          dapp-permissions/v9
+          contact-device-info/v1
+          contact-recovery/v1])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -521,4 +537,7 @@
                :migration     migrations/v35}
               {:schema        v36
                :schemaVersion 36
+               :migration     (constantly nil)}
+              {:schema        v37
+               :schemaVersion 37
                :migration     (constantly nil)}])

dragonmaster-alpha/react-status

commit 1d6e5c6e1cafc03ccd47950f9755ce28baa5ad14

Fix block contact db error
https://github.com/status-im/status-react/commit/bddae03ab234f6574b0c5d4615f586e81512a638
broke adding a user to contacts, this fixes by serializing the contact
before saving it.

Signed-off-by: Andrea Maria Piana <andrea.maria.piana@gmail.com>

src/status_im/data_store/contacts.cljs
@@ -71,7 +71,7 @@
   "Returns tx function for deleting user messages"
   [{:keys [public-key] :as contact} messages-ids]
   (fn [realm]
-    (core/create realm :contact contact true)
+    (core/create realm :contact (serialize-contact contact) true)
     (when-let [user-messages
                (get-messages-by-messages-ids messages-ids)]
       (core/delete realm user-messages))